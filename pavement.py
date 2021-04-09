e46afa6984015f4d0ef7b23c788fa380911a717a
seyi <saadedokun121@gmail.com>
2021-04-09 12:29:11 +0000

    paver file refactored

--- pavement.py
+++ pavement.py
@@ -1,27 +1,29 @@
 from paver.tasks import task, BuildFailure
 from paver.easy import sh, needs
 
+# I am adding python -m for code execution in Jenkins (External server).
+# Not needed in running directly here on linux venv35
 
 @task
 def unit_tests():
-    sh('python -m pytest --cov=bank/bank bank/test/unit')
+    sh('python -m pytest --cov=bank/bank bank/test/unit > coverage.xml')
 
 
 @task
 def py_tests():
-    sh('pytest bank/test/bdd')
+    sh('python -m behave bank/test/bdd')
 
 
 @task
 def run_pylint():
     try:
-        sh('pylint --msg-template="{path}:{line}:{{msg_id}({symbol}), {obj}] {msg}" bank/ > pylint.txt')
+        sh('python -m pylint --msg-template="{path}:{line}:[{msg_id}({symbol}), {obj}] {msg}" bank/ > pylint.txt')
     except BuildFailure:
         pass
 
 
-#@needs('unit_tests', 'py_tests', 'run_pylint')
-@needs('unit_tests')
+@needs('unit_tests', 'py_tests', 'run_pylint')
+#@needs('unit_tests')
 @task
 def default():
     pass
